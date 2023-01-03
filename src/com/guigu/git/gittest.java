d8a871686fc06bf1bd352da6169ec0ce1b56ff1f
董 <1477726053@qq.com>
2023-01-03 22:06:36 +0800

    second

--- src/com/guigu/git/gittest.java
+++ src/com/guigu/git/gittest.java
@@ -3,5 +3,6 @@ package com.guigu.git;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hellow git");
+        System.out.println("hellow git2");
     }
 }
