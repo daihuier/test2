f412ac3df8417e982cf2439a37b964aff10023f8
董 <1477726053@qq.com>
2023-01-03 22:30:46 +0800

    newBthird

--- src/com/guigu/git/gittest.java
+++ src/com/guigu/git/gittest.java
@@ -4,5 +4,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("hellow git");
         System.out.println("hellow git2");
+        System.out.println("hellow git3");
     }
 }
